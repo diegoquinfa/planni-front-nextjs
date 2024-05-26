6e9b02e5a2f627fc611658572430b4d7d1e73450
Jaider808 <babiloniaj@utb.edu.co>
2024-05-26 11:30:26 -0500

    Se agrego la vista detail

--- src/app/detail/page.jsx
+++ src/app/detail/page.jsx
@@ -3,26 +3,57 @@ import Container from '@/components/general/Container'
 const Page = () => {
   return (
     <Container>
-      <div className="flex w-auto flex-col items-center gap-4">
-        <h1 className="text-3xl font-bold sm:text-5xl ">Hotel 1</h1>
-        <div className="grid w-full grid-flow-row grid-cols-2 gap-2 md:grid-cols-3 md:gap-4">
-          <img
-            className="col-span-2 h-full w-full rounded-lg object-cover md:col-span-2 md:row-span-2"
-            src="img\destino-cartagena.jpg"
-            alt="destino"
-          />
-          <img
-            className="row-span-1 h-full w-full rounded-lg object-cover md:col-span-1"
-            src="img\destino-cartagena.jpg"
-            alt="destino"
-          />
-          <img
-            className="row-span-1 h-full w-full rounded-lg object-cover md:row-span-1"
-            src="img\destino-cartagena.jpg"
-            alt="destino"
-          />
+      <div className="flex w-full flex-col items-start gap-12">
+        <div className="flex flex-col gap-3">
+          <h1 className="w-full text-center text-3xl font-bold sm:text-5xl">
+            Hotel 1
+          </h1>
+          <div className="flex md:h-[540px] md:w-full">
+            <img
+              className="h-full w-full rounded-xl object-cover max-sm:rounded-lg"
+              src="img\destino-cartagena.jpg"
+              alt="destino"
+            />
+          </div>
+          <div className="flex w-full flex-col-reverse gap-3 md:p-4 lg:flex-row ">
+            <p className="sm:text-xl lg:w-[60%]">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+              similique in exercitationem qui eos placeat quae saepe deserunt
+              necessitatibus illo officia ex ipsam natus asperiores voluptas at
+              perspiciatis, laboriosam dolores? Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Alias similique in exercitationem
+              qui eos placeat quae saepe deserunt necessitatibus illo officia ex
+              ipsam natus asperiores voluptas at perspiciatis, laboriosam
+              dolores? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Alias similique in exercitationem qui eos placeat quae saepe
+              deserunt necessitatibus illo officia ex ipsam natus asperiores
+              voluptas at perspiciatis, laboriosam dolores?
+            </p>
+            <div className=" flex w-full items-center justify-center max-lg:rounded-xl max-lg:bg-white lg:w-[40%]">
+              <div className="flex flex-col items-center justify-center gap-3 py-6 lg:rounded-xl lg:bg-white lg:px-16 lg:py-20">
+                <p className=" md:text-xl">Total de paquete</p>
+                <h2 className="w-full font-bold max-sm:text-2xl sm:text-4xl">
+                  $ 1.320.000
+                </h2>
+                <button className="rounded-3xl bg-brand px-7 py-2 text-white duration-300 hover:bg-brand-light hover:bg-red-900">
+                  Reservar
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-2">
+        <div className="flex w-full flex-col gap-8 p-3 md:px-4 lg:w-[75%]">
+          <div className="flex items-center justify-between gap-3 rounded-xl bg-white p-2 max-lg:flex-col md:flex-row md:p-4 ">
+            <div>
+              <p className=" md:text-xl">Restaurante</p>
+              <h2 className="text-xl font-bold sm:text-2xl">
+                Nombre del restaurante
+              </h2>
+            </div>
+            <h2 className="font-bold max-sm:text-xl sm:text-2xl ">
+              $ 1.000.000
+            </h2>
+          </div>
           <p className="sm:text-xl">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
             similique in exercitationem qui eos placeat quae saepe deserunt
@@ -36,27 +67,67 @@ const Page = () => {
             necessitatibus illo officia ex ipsam natus asperiores voluptas at
             perspiciatis, laboriosam dolores?
           </p>
-          <div className="grid items-center gap-2 rounded-lg bg-white p-4">
-            <p className="text-xl font-bold">KAYAK</p>
-            <h2 className="text-4xl font-bold">$ 1.320.000</h2>
-            <p className="text-xl font-bold">incluye</p>
-            <div className="flex items-center gap-2">
-              <p className="rounded-full bg-teal-700 px-3 text-white">Vuelos</p>
-              <p className="rounded-full bg-teal-700 px-3 text-white">
-                Transporte al hotel
-              </p>
-              <p className="rounded-full bg-teal-700 px-3 text-white">
-                Estadía
-              </p>
-              <p className="rounded-full bg-teal-700 px-3 text-white">
-                Piscina
+        </div>
+        <div className="flex flex-col items-center gap-11 p-3 md:p-4 ">
+          <h2 className="w-full rounded-xl bg-white py-3 text-center text-xl font-bold sm:text-3xl md:px-4 md:py-2 lg:w-[70%]">
+            Atracciones
+          </h2>
+          <div className="lg:flex-cols-2 flex flex-col gap-4 lg:flex-row">
+            <div className="flex w-full flex-col gap-4 rounded-xl bg-white p-3 md:px-5 md:py-3 lg:w-[50%]">
+              <h2 className="text-center text-xl md:text-2xl font-bold">
+                Lorem, ipsum dolor sit amet consectetur adipisicing
+              </h2>
+              <div className="flex md:h-[300px] md:w-full">
+                <img
+                  className="h-full w-full rounded-xl object-cover max-sm:rounded-lg"
+                  src="img\destino-cartagena.jpg"
+                  alt="destino"
+                />
+              </div>
+              <h2 className="text-center font-bold max-sm:text-xl sm:text-2xl">
+                $ 1.000.000
+              </h2>
+              <p className="sm:text-xl">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+                similique in exercitationem qui eos placeat quae saepe deserunt
+                necessitatibus illo officia ex ipsam natus asperiores voluptas
+                at perspiciatis, laboriosam dolores? Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Alias similique in exercitationem
+                qui eos placeat quae saepe deserunt necessitatibus illo officia
+                ex ipsam natus asperiores voluptas at perspiciatis, laboriosam
+                dolores? Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Alias similique in exercitationem qui eos placeat quae
+                saepe deserunt necessitatibus illo officia ex ipsam natus
+                asperiores voluptas at perspiciatis, laboriosam dolores?
               </p>
             </div>
-            <div className="flex flex-row gap-3">
-              <p>corazon</p>
-              <button className="rounded-3xl bg-brand px-6 py-1 text-white duration-300 hover:bg-brand-light hover:bg-red-900">
-                Reservar
-              </button>
+            <div className="flex w-full flex-col gap-4 rounded-xl bg-white p-3 md:px-5 md:py-3 lg:w-[50%]">
+              <h2 className="text-center text-xl md:text-2xl font-bold">
+                Lorem, ipsum dolor sit amet consectetur adipisicing
+              </h2>
+              <div className="flex md:h-[300px] md:w-full">
+                <img
+                  className="h-full w-full rounded-xl object-cover max-sm:rounded-lg"
+                  src="img\destino-cartagena.jpg"
+                  alt="destino"
+                />
+              </div>
+              <h2 className="text-center font-bold max-sm:text-xl sm:text-2xl">
+                $ 1.000.000
+              </h2>
+              <p className="sm:text-xl">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+                similique in exercitationem qui eos placeat quae saepe deserunt
+                necessitatibus illo officia ex ipsam natus asperiores voluptas
+                at perspiciatis, laboriosam dolores? Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Alias similique in exercitationem
+                qui eos placeat quae saepe deserunt necessitatibus illo officia
+                ex ipsam natus asperiores voluptas at perspiciatis, laboriosam
+                dolores? Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Alias similique in exercitationem qui eos placeat quae
+                saepe deserunt necessitatibus illo officia ex ipsam natus
+                asperiores voluptas at perspiciatis, laboriosam dolores?
+              </p>
             </div>
           </div>
         </div>
